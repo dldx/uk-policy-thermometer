97a6562407cfa8d70e92e42bae0e07aab2680b4e
Durand D'souza <durand1@gmail.com>
2025-11-28 22:13:32 +0000

    fix: Correct relative paths for ground and bedrock images in GroundLayer component

--- src/lib/components/GroundLayer.svelte
+++ src/lib/components/GroundLayer.svelte
@@ -4,7 +4,7 @@
     <!-- Tiled Ground Layer -->
     <div
         class="bg-repeat-x bg-bottom w-full h-[400px] sm:h-[600px] md:h-[800px]"
-        style="background-image: url('/ground.svg'); background-size: auto 100%;"
+        style="background-image: url('./ground.svg'); background-size: auto 100%;"
     ></div>
 
     <!-- Extended bedrock -->
@@ -12,7 +12,7 @@
         <div class="top-10 absolute inset-0 bg-[#656760]"></div>
         <!-- Bedrock Pattern -->
         <div
-            class="absolute inset-0 bg-[url('/bedrock.svg')] bg-position-[20%_0%] sm:bg-position-[-20%_0%] bg-repeat bg-size-[120%_auto] sm:bg-size-[auto_auto]"
+            class="absolute inset-0 bg-[url('./bedrock.svg')] bg-position-[20%_0%] sm:bg-position-[-20%_0%] bg-repeat bg-size-[120%_auto] sm:bg-size-[auto_auto]"
         ></div>
     </div>
 </div>
