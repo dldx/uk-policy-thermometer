8081f22909ecf2a7fdae358f4c153e7cd18b8643
Durand D'souza <durand1@gmail.com>
2025-11-28 22:17:12 +0000

    fix: Update bedrock background style for consistency in GroundLayer component

--- src/lib/components/GroundLayer.svelte
+++ src/lib/components/GroundLayer.svelte
@@ -12,7 +12,8 @@
         <div class="top-10 absolute inset-0 bg-[#656760]"></div>
         <!-- Bedrock Pattern -->
         <div
-            class="absolute inset-0 bg-[url('./bedrock.svg')] bg-position-[20%_0%] sm:bg-position-[-20%_0%] bg-repeat bg-size-[120%_auto] sm:bg-size-[auto_auto]"
+            class="absolute inset-0 bg-position-[20%_0%] sm:bg-position-[-20%_0%] bg-repeat bg-size-[120%_auto] sm:bg-size-[auto_auto]"
+            style="background-image: url('./bedrock.svg');"
         ></div>
     </div>
 </div>
